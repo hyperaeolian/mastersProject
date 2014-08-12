755cf8658e79da7e057e0b1bed2ced12acf0e546
milrob <milrober@gmail.com>
2014-08-12 15:21:09 -0700

    rm sl header

--- Source/AudioApp.cpp
+++ Source/AudioApp.cpp
@@ -396,26 +396,25 @@ void AudioApp::playerStoppedOrStarted(drow::AudioFilePlayer* player){
 void AudioApp::shiftyLooping(){
     for (int i = 0; i < markov_chain.size(); ++i) {
         currentLoop = &crudeLoops[markov_chain[i]];
-        std::thread t1(std::bind(&AudioApp::toggleLoop, this), std::ref(*currentLoop));
-        t1.join();
+//        std::thread t1(std::bind(&AudioApp::toggleLoop, this, this), std::ref(*currentLoop));
+//        t1.join();
         if (markov_chain[i+1] > markov_chain[i]) {
             shifting = forward = true;
-            std::thread t1(std::bind(&AudioApp::toggleLoop, this), std::ref(*currentLoop));
-            t1.join();
         } else if (markov_chain[i+1] < markov_chain[i]) {
             shifting = true;
             forward = false;
-            std::thread t1(std::bind(&AudioApp::toggleLoop, this), std::ref(*currentLoop));
-            t1.join();
+        } else {
+            shifting = false;
         }
-        
+//        std::thread t2(std::bind(&AudioApp::toggleLoop, this, this), std::ref(*currentLoop));
+//        t2.join();
         if (ShiftyLooping != state) break;
     }
 
     
 }
 
-void AudioApp::toggleLoop(Loop &loop){
+void AudioApp::toggleLoop(Loop& loop){
     if (shifting) {
         if (forward) {
             mediaPlayer.setLoopTimes(loop.prev->end, loop.end);
